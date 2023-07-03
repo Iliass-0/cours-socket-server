aaf253e08b8bd553c0f2d281586e68e389a5f4a6
Chris Crognier <christophe.crognier@gmail.com>
2023-07-03 16:37:35 +0200

    Message receive handling

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log(`User connected : ${socket.id}`);
+    
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.broadcast.emit("receive_message", data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected', socket.id)
+    });
 });
 
 server.listen(3001, () => {
