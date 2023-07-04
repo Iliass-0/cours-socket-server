6a1dc35841adeaf3da4c0c4285924c311730f0bd
Chris Crognier <christophe.crognier@gmail.com>
2023-07-04 15:48:11 +0200

    Handle group join

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const {Server} = require('socket.io');
 const cors = require('cors');
-const { Server } = require('socket.io');
 
+//Initialisation du serveur
 app.use(cors);
-const server = http.createServer(app);
-
+const server = http.createServer(app); 
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"],
-    }
+    },
 });
 
-io.on('connection', (socket) => {
+// Socket.io
+io.on("connection", (socket) => {
+    // transmet l'id lors d'une connection
     console.log(`User connected : ${socket.id}`);
-    
-    socket.on("send_message", (data) => {
-        console.log(data);
-        socket.broadcast.emit("receive_message", data);
+
+    // Me permet de rejoindre un groupe/chaine
+    socket.on("join_room", (data) => {
+        socket.join(data);
     });
 
-    socket.on('disconnect', () => {
-        console.log('User Disconnected', socket.id)
+    // Me permet de récupérer les messages du client
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data)
     });
 });
 
+// On lance le serveur
 server.listen(3001, () => {
-    console.log("SERVER IS UP");
+    console.log('SERVER IS RUNNING');
 });
