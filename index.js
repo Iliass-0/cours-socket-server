ad55e21976a6cbedd03b3365f21dea05b0aa8c59
Chris Crognier <christophe.crognier@gmail.com>
2023-07-05 11:52:27 +0200

    Updated server with users management, broadcast bug corrected

--- index.js
+++ index.js
@@ -14,19 +14,29 @@ const io = new Server(server, {
     },
 });
 
+let users = [];
+
 // Socket.io
 io.on("connection", (socket) => {
     // transmet l'id lors d'une connection
     console.log(`User connected : ${socket.id}`);
 
+    socket.on('new_user', (data) => {
+      users.push(data);
+      console.log(users);
+      io.emit('receive_users', users);
+    });
+
     // Me permet de récupérer les messages du client
     socket.on("send_message", (data) => {
       console.log(data);
-      socket.emit("receive_message", data)
+      io.emit("receive_message", data)
     });
 
     socket.on('disconnect', () => {
       console.log('A user disconnected');
+      users = users.filter((user) => user.socketID !== socket.id);
+      socket.emit('receive_users', users);
       socket.disconnect();
     });
 });
